9ce391a8facb59a2491514e9b5f63157e0cfa279
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-03-08 05:12:30 -0300

    Feature: Page NotFound

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import About from './Pages/About';
 import Home from './Pages/Home';
+import NotFound from './Pages/NotFound';
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
+      <Route component={NotFound} />
     </Switch>
   );
 };
