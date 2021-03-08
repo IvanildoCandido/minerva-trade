368f232418c2a8943e2563b58e4dad35f2773c60
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-03-07 21:49:18 -0300

    Feature: Header Component

--- frontend/src/components/partials/Header/index.js
+++ frontend/src/components/partials/Header/index.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import { HeaderArea } from './styled';
+import { Link } from 'react-router-dom';
+import { isLogged } from '../../../helpers/AuthHandler';
+import logo from '../../../assets/images/logo.svg';
 
 const Header = () => {
-    return (
-        <HeaderArea>
-            ...
-        </HeaderArea>
-    );
-}
+  let logged = isLogged();
+  return (
+    <HeaderArea>
+      <div className="container">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="Logotipo Minerva" />
+          </Link>
+        </div>
+        <nav>
+          <ul>
+            {logged && (
+              <>
+                <li>
+                  <Link to="/my-account"> Minha Conta</Link>
+                </li>
+                <li>
+                  <Link to="/logout"> Sair</Link>
+                </li>
+                <li>
+                  <Link to="/post-an-ad" className="button">
+                    Postar anúncio
+                  </Link>
+                </li>
+              </>
+            )}
+            {!logged && (
+              <>
+                <li>
+                  <Link to="/signin"> Login</Link>
+                </li>
+                <li>
+                  <Link to="/signup"> Cadastrar</Link>
+                </li>
+                <li>
+                  <Link to="/signin" className="button">
+                    Postar anúncio
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
+      </div>
+    </HeaderArea>
+  );
+};
 
 export default Header;
--- frontend/src/components/partials/Header/styled.js
+++ frontend/src/components/partials/Header/styled.js
@@ -1,6 +1,59 @@
 import styled from 'styled-components';
 
 export const HeaderArea = styled.div`
-    height:100px;
-    background-color:#CCC;
+  background-color: #fff;
+  height: 60px;
+  border-bottom: 1px solid #ccc;
+  a {
+    text-decoration: none;
+  }
+  .container {
+    max-width: 1000px;
+    margin: auto;
+    display: flex;
+  }
+  .logo {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    height: 60px;
+    img {
+      height: 50px;
+    }
+  }
+  nav {
+    padding-top: 10px;
+    padding-bottom: 10px;
+    ul,
+    li {
+      margin: 0;
+      padding: 0;
+      list-style: none;
+    }
+    ul {
+      display: flex;
+      align-items: center;
+      height: 40px;
+    }
+    li {
+      margin-left: 20px;
+      margin-right: 20px;
+      a {
+        color: #000;
+        font-size: 14px;
+        &:hover {
+          color: #999;
+        }
+        &.button {
+          background-color: #ff8100;
+          border-radius: 5px;
+          color: #fff;
+          padding: 5px 10px;
+          &:hover {
+            background-color: #e57706;
+          }
+        }
+      }
+    }
+  }
 `;
