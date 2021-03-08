67d95d04b74a4d9fa07c97cf62079122825101cb
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-03-08 08:58:44 -0300

    Feature: Signin

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 import About from './Pages/About';
 import Home from './Pages/Home';
 import NotFound from './Pages/NotFound';
+import Signin from './Pages/Signin';
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/signin" component={Signin} />
       <Route exact path="/about" component={About} />
       <Route component={NotFound} />
     </Switch>
