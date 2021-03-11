455d2cb3e664f966d92b0611352aad4647830ffc
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-03-11 07:05:25 -0300

    Feature: Implements BreadCrumb

--- frontend/src/Pages/AdPage/index.js
+++ frontend/src/Pages/AdPage/index.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Slide } from 'react-slideshow-image';
+import { Link, useParams } from 'react-router-dom';
 import { PageContainer } from '../../components/MainComponents';
-import { PageArea, Fake } from './styled';
+import { PageArea, Fake, OthersArea, BreadCrumb } from './styled';
+import AdItem from '../../components/partials/AdItem';
 import MinervaAPI from '../../helpers/MinervaTradeAPI';
+import 'react-slideshow-image/dist/styles.css';
 
 const AdPage = () => {
   const API = MinervaAPI();
@@ -23,10 +26,34 @@ const AdPage = () => {
   };
   return (
     <PageContainer>
+      {adInfo.category && (
+        <BreadCrumb>
+          Você está aqui:
+          <Link to="/">Home</Link>/
+          <Link to={`/ads?state=${adInfo.stateName}`}>{adInfo.stateName}</Link>/
+          <Link
+            to={`/ads?state=${adInfo.stateName}&cat=${adInfo.category.slug}`}
+          >
+            {adInfo.category.name}
+          </Link>
+          / {adInfo.title}
+        </BreadCrumb>
+      )}
       <PageArea>
         <div className="left-side">
           <div className="box">
-            <div className="ad-image">{loading && <Fake height={300} />}</div>
+            <div className="ad-image">
+              {loading && <Fake height={300} />}
+              {adInfo.images && (
+                <Slide>
+                  {adInfo.images.map((image, index) => (
+                    <div key={index} className="each-slide">
+                      <img src={image} alt="" />
+                    </div>
+                  ))}
+                </Slide>
+              )}
+            </div>
             <div className="ad-info">
               <div className="ad-name">
                 {loading && <Fake height={20} />}
@@ -47,12 +74,45 @@ const AdPage = () => {
         <div className="right-side">
           <div className="box box-padding">
             {loading && <Fake height={20} />}
+            {adInfo.priceNegotiable && 'Preço Negociável'}
+            {!adInfo.priceNegotiable && adInfo.price && (
+              <div className="price">
+                Preço: <span>R$ {adInfo.price}</span>
+              </div>
+            )}
           </div>
-          <div className="box box-padding">
-            {loading && <Fake height={50} />}
-          </div>
+          {loading && <Fake height={50} />}
+          {adInfo.userInfo && (
+            <>
+              <a
+                href={`mailto:${adInfo.userInfo.email}`}
+                target="_blank"
+                className="contact-seller"
+                rel="noreferrer"
+              >
+                Conversar com o vendedor
+              </a>
+              <div className="box box-padding created-by">
+                <strong>{adInfo.userInfo.name}</strong>
+                <small>E-mail: {adInfo.userInfo.email}</small>
+                <small>Estado: {adInfo.stateName}</small>
+              </div>
+            </>
+          )}
         </div>
       </PageArea>
+      <OthersArea>
+        {adInfo.others && (
+          <>
+            <h2>Outros anúncios do vendedor</h2>
+            <div className="list">
+              {adInfo.others.map((item, index) => (
+                <AdItem key={index} data={item} />
+              ))}
+            </div>
+          </>
+        )}
+      </OthersArea>
     </PageContainer>
   );
 };
--- frontend/src/Pages/AdPage/styled.js
+++ frontend/src/Pages/AdPage/styled.js
@@ -13,6 +13,7 @@ export const PageArea = styled.div`
     border-radius: 5px;
     box-shadow: 0, 0, 4 #999;
     margin-bottom: 20px;
+    padding: 30px;
   }
   .box-padding {
     padding: 10px;
@@ -20,18 +21,90 @@ export const PageArea = styled.div`
   .left-side {
     flex: 1;
     margin-right: 20px;
+    .box {
+      display: flex;
+    }
     .ad-image {
+      width: 320px;
+      height: 320px;
+      margin-right: 20px;
+
+      .each-slide img {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-size: cover;
+        height: 320px;
+      }
     }
     .ad-info {
-      padding: 10px;
+      flex: 1;
+      padding: 20px;
       .ad-name {
         margin-bottom: 20px;
+        h2 {
+          margin: 0;
+          margin-top: 20px;
+        }
+        small {
+          color: #999;
+        }
       }
       .ad-description {
+        small {
+          color: #999;
+        }
       }
     }
   }
   .right-side {
     width: 250px;
+    .price span {
+      color: blue;
+      display: block;
+      font-size: 27px;
+      font-weight: bold;
+    }
+    .contact-seller {
+      background-color: blue;
+      color: #fff;
+      height: 30px;
+      border-radius: 5px;
+      box-shadow: 0, 0, 4px #999;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-decoration: none;
+      margin-bottom: 20px;
+    }
+    .created-by strong {
+      display: block;
+    }
+    .created-by small {
+      display: block;
+      color: #999;
+      margin-top: 10px;
+    }
+  }
+`;
+export const OthersArea = styled.div`
+  h2 {
+    font-size: 20px;
+  }
+  .list {
+    display: flex;
+    .ad-item {
+      width: 25%;
+    }
+  }
+`;
+export const BreadCrumb = styled.div`
+  font-size: 13px;
+  margin-top: 20px;
+  a {
+    display: inline-block;
+    margin: 0 5px;
+    text-decoration: underline;
+    color: black;
   }
 `;
